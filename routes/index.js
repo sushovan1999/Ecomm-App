3fc62f09b40e7ab9ae3bca5baaffed6a398b9891
Sushovan Roy <33160274+sushovan1999@users.noreply.github.com>
2022-11-24 00:34:30 +0530

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 let express = require("express");
 let router = express.Router();
-let categoryController = require("./../controller/category.controller");
 let categoryRoutes = require("./categories.route");
 let productRoutes = require("./products.route");
 let authRoute = require("./auth.route");
@@ -8,8 +7,8 @@ router.get("/", (req, res, next) => {
   res.write("this is the base page");
   res.end();
 });
-router.use("/categories", categoryRoutes);
-router.use("/products", productRoutes);
-router.use("/auth", authRoute);
+router.use("/ecomm/api/v1/categories", categoryRoutes);
+router.use("/ecomm/api/v1/products", productRoutes);
+router.use("/ecomm/api/v1/auth", authRoute);
 
 module.exports = router;
