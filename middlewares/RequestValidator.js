9fe8346a105312b62d3a3d195333a2fed5005dee
Sushovan Roy <33160274+sushovan1999@users.noreply.github.com>
2022-11-24 12:05:17 +0530

    validator issue solved

--- middlewares/RequestValidator.js
+++ middlewares/RequestValidator.js
@@ -5,25 +5,28 @@ const validateReqForCategoryName = (req, res, next) => {
     res.status(400).send({
       message: "Category name is required",
     });
+    return;
   }
   next();
 };
 
 const validateReqForCategoryId = async (req, res, next) => {
-  let categoryId = req.params.categoryId;
-  if (categoryId) {
-    let category = await Categories.findByPk(categoryId);
+  let CategoryId = req.params.categoryId;
+  if (CategoryId) {
+    let category = await Categories.findByPk(CategoryId);
     if (!category) {
       res.status(400).send({
         message: "Category does not exist",
       });
+      return;
     }
+    next();
   } else {
     res.status(400).send({
       message: "Category id is missing",
     });
   }
-  res.end();
+  // res.end();
 };
 
 module.exports = { validateReqForCategoryName, validateReqForCategoryId };
